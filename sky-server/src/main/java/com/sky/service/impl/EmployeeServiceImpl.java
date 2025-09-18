bd9907ba85e2f894122e78ee20e09c814522a4b9
“zhenghaotian” <578672788@qq.com>
2025-09-18 15:35:42 +0800

    启用禁用员工代码

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -113,4 +113,22 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     }
 
+
+
+    /**
+     * 启用禁用员工账号
+     *
+     * @param status
+     * @param id
+     */
+    public void startOrStop(Integer status, Long id) {
+        Employee employee = Employee.builder()
+                .status(status)
+                .id(id)
+                .build();
+
+        employeeMapper.update(employee);
+    }
+
+
 }
